5bdaa14c8af1f8649341558cf600bd3c472db4ea
Migur Maxim <72813412+mmigur@users.noreply.github.com>
2021-09-29 01:13:35 +0600

    Add files via upload

---
+++ TreatmentImage/TreatmentImage/TreatmentImage/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿
+namespace TreatmentImage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SplitContainer = new System.Windows.Forms.SplitContainer();
+            this.ImageBox = new System.Windows.Forms.PictureBox();
+            this.TrackBar = new System.Windows.Forms.TrackBar();
+            this.Menu = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.OpenFile = new System.Windows.Forms.OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).BeginInit();
+            this.SplitContainer.Panel1.SuspendLayout();
+            this.SplitContainer.Panel2.SuspendLayout();
+            this.SplitContainer.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrackBar)).BeginInit();
+            this.Menu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // SplitContainer
+            // 
+            this.SplitContainer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.SplitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.SplitContainer.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.SplitContainer.IsSplitterFixed = true;
+            this.SplitContainer.Location = new System.Drawing.Point(0, 24);
+            this.SplitContainer.Name = "SplitContainer";
+            // 
+            // SplitContainer.Panel1
+            // 
+            this.SplitContainer.Panel1.Controls.Add(this.ImageBox);
+            // 
+            // SplitContainer.Panel2
+            // 
+            this.SplitContainer.Panel2.Controls.Add(this.TrackBar);
+            this.SplitContainer.Size = new System.Drawing.Size(1206, 678);
+            this.SplitContainer.SplitterDistance = 1144;
+            this.SplitContainer.TabIndex = 0;
+            // 
+            // ImageBox
+            // 
+            this.ImageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ImageBox.Location = new System.Drawing.Point(0, 0);
+            this.ImageBox.Name = "ImageBox";
+            this.ImageBox.Size = new System.Drawing.Size(1140, 674);
+            this.ImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ImageBox.TabIndex = 0;
+            this.ImageBox.TabStop = false;
+            // 
+            // TrackBar
+            // 
+            this.TrackBar.Dock = System.Windows.Forms.DockStyle.Right;
+            this.TrackBar.Location = new System.Drawing.Point(9, 0);
+            this.TrackBar.Maximum = 100;
+            this.TrackBar.Minimum = 1;
+            this.TrackBar.Name = "TrackBar";
+            this.TrackBar.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.TrackBar.Size = new System.Drawing.Size(45, 674);
+            this.TrackBar.TabIndex = 1;
+            this.TrackBar.Value = 1;
+            this.TrackBar.Scroll += new System.EventHandler(this.TrackBar_Scroll);
+            // 
+            // Menu
+            // 
+            this.Menu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.Menu.Location = new System.Drawing.Point(0, 0);
+            this.Menu.Name = "Menu";
+            this.Menu.Size = new System.Drawing.Size(1206, 24);
+            this.Menu.TabIndex = 1;
+            this.Menu.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // OpenFile
+            // 
+            this.OpenFile.Filter = "Images|*.bmp;*.png;*.jpg";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1206, 702);
+            this.Controls.Add(this.SplitContainer);
+            this.Controls.Add(this.Menu);
+            this.MainMenuStrip = this.Menu;
+            this.Name = "Form1";
+            this.Text = "0%";
+            this.SplitContainer.Panel1.ResumeLayout(false);
+            this.SplitContainer.Panel2.ResumeLayout(false);
+            this.SplitContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SplitContainer)).EndInit();
+            this.SplitContainer.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.TrackBar)).EndInit();
+            this.Menu.ResumeLayout(false);
+            this.Menu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer SplitContainer;
+        private System.Windows.Forms.PictureBox ImageBox;
+        private System.Windows.Forms.TrackBar TrackBar;
+        private System.Windows.Forms.MenuStrip Menu;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog OpenFile;
+    }
+}
+
---
+++ TreatmentImage/TreatmentImage/TreatmentImage/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TreatmentImage
+{
+    public partial class Form1 : Form
+    {
+        private List<Bitmap> _bitmaps = new List<Bitmap>();
+        private Random _random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (OpenFile.ShowDialog() == DialogResult.OK)
+            {
+                var stopWatch = Stopwatch.StartNew();
+                Menu.Enabled = TrackBar.Enabled = false;
+                ImageBox.Image = null;
+                _bitmaps.Clear();
+                var bitmap = new Bitmap(OpenFile.FileName);
+                await Task.Run(() => { RunProccesing(bitmap); });
+                Menu.Enabled = TrackBar.Enabled = true;
+                stopWatch.Stop();
+                Text = stopWatch.Elapsed.ToString();
+            }
+        }
+
+        private void RunProccesing(Bitmap bitmap)
+        {
+            var pixels = GetPixels(bitmap);
+            var pixelsInStep = (bitmap.Width * bitmap.Height) / 100;
+            var currenrPixelsSet = new List<Pixel>(pixels.Count - pixelsInStep);
+
+            for (int i = 0; i < TrackBar.Maximum; i++)
+            {
+                for (int j = 0; j < pixelsInStep; j++)
+                {
+                    var index = _random.Next(pixels.Count);
+                    currenrPixelsSet.Add(pixels[index]);
+                    pixels.RemoveAt(index);
+                }
+                var currentBitmap = new Bitmap(bitmap.Width, bitmap.Height);
+
+                foreach (var pixel in currenrPixelsSet)
+                    currentBitmap.SetPixel(pixel.Point.X, pixel.Point.Y, pixel.Color);
+                _bitmaps.Add(currentBitmap);
+
+                this.Invoke(new Action(() => { Text = $"{i}%"; }));
+            }
+            _bitmaps.Add(bitmap);
+        }
+
+        private List<Pixel> GetPixels(Bitmap bitmap)
+        {
+            var pixels = new List<Pixel>(bitmap.Width * bitmap.Height);
+
+            for (int y = 0; y < bitmap.Height; y++)
+            {
+                for (int x = 0; x < bitmap.Width; x++)
+                {
+                    pixels.Add(new Pixel()
+                    {
+                        Color = bitmap.GetPixel(x, y),
+                        Point = new Point() { X = x, Y = y }
+                    });
+                }
+            }
+            return pixels;
+        }
+
+        private void TrackBar_Scroll(object sender, EventArgs e)
+        {
+            if (_bitmaps == null || _bitmaps.Count == 0)
+            {
+                return;
+            }
+            ImageBox.Image = _bitmaps[TrackBar.Value - 1];
+        }
+    }
+}
